487418da05ffa08cd68b04c8bf4f17d94b08022e
Alexander C <alexander.culum@gmail.com>
2021-12-16 11:38:15 +0100

    Add admin page and role.

--- unsecured/src/main/java/com/example/securingweb/MvcConfig.java
+++ unsecured/src/main/java/com/example/securingweb/MvcConfig.java
@@ -11,7 +11,6 @@ public class MvcConfig implements WebMvcConfigurer {
 		registry.addViewController("/home").setViewName("home");
 		registry.addViewController("/").setViewName("home");
 		registry.addViewController("/hello").setViewName("hello");
-		registry.addViewController("/login").setViewName("login");
 	}
 
 }
